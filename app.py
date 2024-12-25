dc991d8fddd1cbb73a469d744c3233a337ad4f5d
lixiong <lixiong0610@163.com>
2024-12-25 20:49:08 +0800

    update_my_flask

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 #初始化一个flask实例
 app = Flask(__name__)
 
 #app.route装饰器映射到URL和执行的函数，这个设置将根URL映射到hello_world函数上
-@app.route('/)
+@app.route('/')
 def hello_world():
-  return "Hello World!"
+    return "Hello World!"
+
+@app.route('/index')
+def index_url():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000)
